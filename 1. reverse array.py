dd4f3a5f3fb5907abae12da8b1ae69e00a17349f
DikshaHub <66899235+DikshaHub@users.noreply.github.com>
2021-10-17 10:23:24 +0530

    Update 1. reverse array.py

--- 1. reverse array.py
+++ 1. reverse array.py
@@ -10,14 +10,19 @@ def Reverse(list,start,end):
 
 
 n = int(input(" Please enter size of the array : "))
-print(" Please enter the values of array in a single line using space : ")
+print(" Please enter the values of array in a single line separated space : ")
 
 Array = [n]
 Array = list(map(int, input().split()))
 
-print(" Entered array is : ", Array)
+if len(Array) == n:
+    print(" Given array is "+ str(Array))
+    print(" Reveresd array  is : " + str(Reverse(Array, 0, n - 1)))
 
-print(" Reveresd array  is : " + str(Reverse(Array, 0, n-1)))
+else:
+    print(" Enter "+ str(n) + " elements only. ")
+
+
 
 # Time Complexity O(n)
 
